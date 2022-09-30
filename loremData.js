d858653a543a04a1bf612132b8c2ea42a88d95c3
Kelvin-Squishy <obengappiah98@gmail.com>
2022-09-30 14:51:57 +0800

    complete app functionalities

---
+++ loremData.js
@@ -0,0 +1,3 @@
+const loremIpsum = [];
+
+export default loremIpsum;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,50 @@
 import styles from '../styles/Home.module.css';
 import { useState } from 'react';
 
+import data from '../data';
+
 export default function Home() {
-  return <h1>Hey</h1>;
+  const [number, setNumber] = useState(0);
+
+  const [text, setText] = useState([]);
+
+  const getNumber = (event) => {
+    setNumber(event.target.value);
+  };
+
+  const generateParagraph = (event) => {
+    event.preventDefault();
+
+    let numOfPragraphs = parseInt(number);
+
+    if (numOfPragraphs <= 0) numOfPragraphs = 0;
+
+    setText([data.slice(0, numOfPragraphs)]);
+
+    console.log(text);
+
+    console.log(typeof numOfPragraphs, number);
+  };
+
+  return (
+    <section className={`${styles.main} ${styles.spacing}`}>
+      <h1 className={styles.heading}>tired of boring lorem ipsum?</h1>
+      <form onSubmit={generateParagraph}>
+        <label htmlFor="number">Paragraphs:</label>
+        <input
+          type="number"
+          id="number"
+          name="number"
+          value={number}
+          onChange={getNumber}
+        />
+        <button>Generate</button>
+      </form>
+      <article className={styles.paragraphContainer}>
+        {text.map((paragraph, index) => {
+          return <p key={index}>{paragraph}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
